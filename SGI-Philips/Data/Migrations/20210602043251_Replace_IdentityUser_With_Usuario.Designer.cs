dd4f5a836faeea64eb06c12d74a7690d8bd1059b
PDePaz <pvelasquez@applaudostudios.com>
2021-06-01 22:43:21 -0600

    :card_file_box: 16 Se añadió Usario que hereda de IdentityUser, pero agregandole los campos que queremos que tenga el usuario extra

--- SGI-Philips/Data/Migrations/20210602043251_Replace_IdentityUser_With_Usuario.Designer.cs
+++ SGI-Philips/Data/Migrations/20210602043251_Replace_IdentityUser_With_Usuario.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SGI_Philips.Data;
 
 namespace SGI_Philips.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210602043251_Replace_IdentityUser_With_Usuario")]
+    partial class Replace_IdentityUser_With_Usuario
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
